14838345af6bf3f76e2f6120d7e90dcaf0c2a106
赵玉胜 <472051471@qq.com>
2019-03-30 16:55:49 +0800

    初始提交

---
+++ src/main/java/com/qianfeng/fxmall/commons/info/SystemConstantsUtils.java
@@ -0,0 +1,15 @@
+package com.qianfeng.fxmall.commons.info;
+
+public class SystemConstantsUtils {
+
+
+    /**
+     * 分页相关常量
+     */
+    public static final class Page{
+        /**
+         * 分页大小
+         */
+        public final static Integer PAGE_SIZE=5;
+    }
+}
